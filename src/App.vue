365b73f0fbfdb293bdbf864a98243df3c5bdf473
Lucas Lessa <lucaslessa14@hotmail.com>
2020-09-28 23:10:00 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,330 @@
+<template>
+  <div id="app">
+    <div class="box-resistor">
+      <Resistor 
+        :color1="faixa1"
+        :color2="faixa2"
+        :color3="faixa3"
+        :color4="faixa4"
+        :color5="faixa5"
+      />
+
+      <h2>{{ resistencia }} &#8486; {{ faixa5 | calculaTolerancia }}</h2>
+    </div>
+
+    <div class="box-forms">
+      <div class="forms">
+        <h3>Faixa 1</h3>
+        <ul>
+          <li><div class="color color-0" @click="faixa1update(0)"></div></li>
+          <li><div class="color color-1" @click="faixa1update(1)"></div></li>
+          <li><div class="color color-2" @click="faixa1update(2)"></div></li>
+          <li><div class="color color-3" @click="faixa1update(3)"></div></li>
+          <li><div class="color color-4" @click="faixa1update(4)"></div></li>
+          <li><div class="color color-5" @click="faixa1update(5)"></div></li>
+          <li><div class="color color-6" @click="faixa1update(6)"></div></li>
+          <li><div class="color color-7" @click="faixa1update(7)"></div></li>
+          <li><div class="color color-8" @click="faixa1update(8)"></div></li>
+          <li><div class="color color-9" @click="faixa1update(9)"></div></li>
+        </ul>
+      </div>
+
+      <div class="forms">
+        <h3>Faixa 2</h3>
+        <ul>
+          <li><div class="color color-0" @click="faixa2update(0)"></div></li>
+          <li><div class="color color-1" @click="faixa2update(1)"></div></li>
+          <li><div class="color color-2" @click="faixa2update(2)"></div></li>
+          <li><div class="color color-3" @click="faixa2update(3)"></div></li>
+          <li><div class="color color-4" @click="faixa2update(4)"></div></li>
+          <li><div class="color color-5" @click="faixa2update(5)"></div></li>
+          <li><div class="color color-6" @click="faixa2update(6)"></div></li>
+          <li><div class="color color-7" @click="faixa2update(7)"></div></li>
+          <li><div class="color color-8" @click="faixa2update(8)"></div></li>
+          <li><div class="color color-9" @click="faixa2update(9)"></div></li>
+        </ul>
+      </div>
+
+      <div class="forms">
+        <h3>Faixa 3</h3>
+        <ul>
+          <li><div class="color color-0" @click="faixa3update(0)"></div></li>
+          <li><div class="color color-1" @click="faixa3update(1)"></div></li>
+          <li><div class="color color-2" @click="faixa3update(2)"></div></li>
+          <li><div class="color color-3" @click="faixa3update(3)"></div></li>
+          <li><div class="color color-4" @click="faixa3update(4)"></div></li>
+          <li><div class="color color-5" @click="faixa3update(5)"></div></li>
+          <li><div class="color color-6" @click="faixa3update(6)"></div></li>
+          <li><div class="color color-7" @click="faixa3update(7)"></div></li>
+          <li><div class="color color-8" @click="faixa3update(8)"></div></li>
+          <li><div class="color color-9" @click="faixa3update(9)"></div></li>
+        </ul>
+      </div>
+
+      <div class="forms">
+        <h3>Multiplicador</h3>
+        <ul>
+          <li><div class="color color-0" @click="faixa4update(0)"></div></li>
+          <li><div class="color color-1" @click="faixa4update(1)"></div></li>
+          <li><div class="color color-2" @click="faixa4update(2)"></div></li>
+          <li><div class="color color-3" @click="faixa4update(3)"></div></li>
+          <li><div class="color color-4" @click="faixa4update(4)"></div></li>
+          <li><div class="color color-5" @click="faixa4update(5)"></div></li>
+          <li><div class="color color-6" @click="faixa4update(6)"></div></li>
+          <li><div class="color color-7" @click="faixa4update(7)"></div></li>
+          <li><div class="color color-10" @click="faixa4update(10)"></div></li>
+          <li><div class="color color-11" @click="faixa4update(11)"></div></li>
+        </ul>
+      </div>
+
+      <div class="forms">
+        <h3>Tolerância</h3>
+        <ul>
+          <li><div class="color color-1" @click="faixa5update(1)"></div></li>
+          <li><div class="color color-2" @click="faixa5update(2)"></div></li>
+          <li><div class="color color-5" @click="faixa5update(5)"></div></li>
+          <li><div class="color color-6" @click="faixa5update(6)"></div></li>
+          <li><div class="color color-7" @click="faixa5update(7)"></div></li>
+          <li><div class="color color-8" @click="faixa5update(8)"></div></li>
+          <li><div class="color color-10" @click="faixa5update(10)"></div></li>
+          <li><div class="color color-11" @click="faixa5update(11)"></div></li>
+        </ul>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Resistor from './components/Resistor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Resistor
+  },
+  data() {
+    return {
+      faixa1: 2,
+      faixa2: 2,
+      faixa3: 0,
+      faixa4: 0,
+      faixa5: 10,
+      resistencia: '220'
+    }
+  },
+  methods: {
+    faixa1update (color) {
+      this.faixa1 = color;
+      this.updateResistencia();
+    },
+    faixa2update (color) {
+      this.faixa2 = color;
+      this.updateResistencia();
+    },
+    faixa3update (color) {
+      this.faixa3 = color;
+      this.updateResistencia();
+    },
+    faixa4update (color) {
+      this.faixa4 = color;
+      this.updateResistencia();
+    },
+    faixa5update (color) {
+      this.faixa5 = color;
+      this.updateResistencia();
+    },
+    updateResistencia() {
+      this.resistencia = '' + this.faixa1 + this.faixa2 + this.faixa3 + this.calculaMultiplicador(this.faixa4);      
+    },
+    calculaMultiplicador(num) {
+      if (num === 0) {
+        return ''
+      } else if (num === 1) {
+        return '0'
+      } else if (num === 2) {
+        return '00'
+      } else if (num === 3) {
+        return 'K'
+      } else if (num === 4) {
+        return '0K'
+      } else if (num === 5) {
+        return '00K'
+      } else if (num === 6) {
+        return 'M'
+      } else if (num === 7) {
+        return '0M'
+      } else if (num === 10) {
+        return 'x0.1'
+      } else if (num === 11) {
+        return 'x0.01'
+      }
+    },
+    calculaTolerancia(num) {
+      if (num === 1) {
+        return '+/- 1%'
+      } else if (num === 2) {
+        return '+/- 2%'
+      } else if (num === 5) {
+        return '+/- 0.5%'
+      } else if (num === 6) {
+        return '+/- 0.22%'
+      } else if (num === 7) {
+        return '+/- 0.01%'
+      } else if (num === 8) {
+        return '+/- 0.05%'
+      } else if (num === 10) {
+        return '+/- 5%'
+      } else if (num === 11) {
+        return '+/- 10%'
+      }
+    }
+  },
+  filters: {
+    calculaTolerancia: (num) => {
+      if (num === 1) {
+        return '+/- 1%'
+      } else if (num === 2) {
+        return '+/- 2%'
+      } else if (num === 5) {
+        return '+/- 0.5%'
+      } else if (num === 6) {
+        return '+/- 0.22%'
+      } else if (num === 7) {
+        return '+/- 0.01%'
+      } else if (num === 8) {
+        return '+/- 0.05%'
+      } else if (num === 10) {
+        return '+/- 5%'
+      } else if (num === 11) {
+        return '+/- 10%'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+
+  position: absolute;
+  height: 100%;
+  width: 100%;
+}
+
+
+.box-resistor {
+  height: 100%;
+  width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.box-forms {
+  height: 100%;
+  width: 40%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-left: 32px;
+}
+
+@media (max-width: 900px) {
+  #app {
+    flex-direction: column;
+    height: auto;
+  }
+
+  .box-resistor {
+    height: auto;
+    width: 90%;
+    margin: 32px 5%;
+  }
+
+  .box-forms {
+    height: auto;
+    width: 90%;
+    margin: 32px 5%;
+  }
+}
+
+.forms {
+  width: 100%;
+  background: #eee;
+  padding: 16px 8px;
+  margin-bottom: 32px;
+}
+
+.forms ul {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  flex-direction: row;
+  list-style: none;
+}
+
+.color {
+  background: blue;
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  margin: 8px 8px 0 0;
+}
+
+.color-0 {
+ background: black;
+}
+.color-1 {
+  background: brown;  
+}
+.color-2 {
+  background: red;
+}
+.color-3 {
+  background: orange;
+}
+.color-4 {
+  background: yellow;
+}
+.color-5 {
+  background: green;
+}
+.color-6 {
+  background: blue;
+}
+.color-7 {
+  background: violet;
+}
+.color-8 {
+  background: gray;
+}
+.color-9 {
+  background: white;
+}
+.color-10 {
+  background: gold;  
+}
+.color-11 {
+  background: silver;  
+}
+</style>
---
+++ src/components/Resistor.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="resistor">
+    <svg style="width:100%;height:auto;" viewBox="0 0 734 625" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g filter="url(#filter0_i)">
+      <path d="M569.425 610.811L109.212 38.6393C109.212 38.6393 101.983 26.8556 115.474 12.496C128.966 -1.86356 140.915 7.70001 140.915 7.70001L632.238 568.895C632.238 568.895 631.521 596.366 609.126 609.431C586.73 622.495 569.425 610.811 569.425 610.811Z" fill="#C4C4C4" fill-opacity="0.63"/>
+      <path d="M569.425 610.811L109.212 38.6393C109.212 38.6393 101.983 26.8556 115.474 12.496C128.966 -1.86356 140.915 7.70001 140.915 7.70001L632.238 568.895C632.238 568.895 631.521 596.366 609.126 609.431C586.73 622.495 569.425 610.811 569.425 610.811Z" fill="url(#paint0_linear)"/>
+      </g>
+      <g filter="url(#filter1_i)">
+      <path d="M345.103 148.814C364.852 167.785 368.987 179.665 366.792 184.166L374.776 192.801L385.691 204.608L467.253 292.833C473.412 292.297 480.002 296.724 482.528 299.004L513.244 328.141C529.829 342.835 538.162 357.006 540.256 362.254C552.206 387.901 532.458 426.283 521.09 442.268L483.111 397.989C446.28 397.879 435.248 426.454 434.335 440.755L469.368 486.841C457.406 492.018 439.568 497.015 432.144 498.867C396.633 504.434 377.714 489.46 372.694 481.277L332.172 419.635C329.572 416.5 328.81 412.687 330.711 411.746L257.307 316.021L249.647 306.032L239.435 292.714C237.23 293.526 233.487 290.731 231.892 289.232C228.764 285.97 217.588 273.409 197.907 249.261C178.226 225.112 180.456 196.216 184.031 184.787C186.127 177.613 191.118 166.449 199.485 154.982C208.92 142.053 222.646 128.74 241.354 120.327C242.873 119.644 244.425 118.994 246.009 118.378C284.431 103.456 308.385 112.097 315.559 118.283C321.614 124.48 335.999 139.262 345.103 148.814Z" fill="#FEDD77"/>
+      </g>
+      <path d="M256.872 313.966L244.666 298.477C245.507 266.904 266.778 221.666 311.858 199.81C328.577 191.704 346.448 188.473 370.819 187.965L384.799 203.041C360.529 199.715 341.036 202.136 322.256 212.353C283.298 233.547 256.129 279.31 256.872 313.966Z" :fill="color1 | convertColor"/>
+      <path d="M282.299 347.699L268.888 330.247C269.725 298.82 286.409 244.43 332.242 223.121C349.533 215.082 369.132 212.747 396.275 216.679L409.97 230.849C381.549 226.073 361.143 227.58 343.433 235.854C295.408 258.288 280.712 318.555 282.299 347.699Z" :fill="color2 | convertColor"/>
+      <path d="M304.714 376.523L290.511 358.88C293.489 328.802 310.012 273.147 355.036 250.36C372.481 241.532 392.569 238.515 420.241 242.523L434.221 257.599C406.019 250.844 385.135 252.149 366.417 262.301C322.038 286.368 301.09 347.375 304.714 376.523Z" :fill="color3 | convertColor"/>
+      <path d="M326.336 405.157L312.735 388.496C312.413 355.917 338.398 303.188 381.602 279.34C400.753 268.769 418.298 265.058 445.919 269.616L458.504 283.52C431.479 277.047 411.696 279.341 391.398 290.901C349.456 314.787 322.925 369.906 326.336 405.157Z" :fill="color4 | convertColor"/>
+      <path d="M359.695 461.731L347.456 442.884C347.133 410.305 374.489 357.905 417.693 334.057C436.845 323.486 469.902 308.412 497.522 312.969L513.879 328.615C486.854 322.142 447.787 334.058 427.489 345.618C385.547 369.504 356.284 426.481 359.695 461.731Z" :fill="color5 | convertColor"/>
+      <path d="M315.559 118.283C321.614 124.48 335.999 139.262 345.103 148.814C325.092 156.586 289.825 177.458 308.846 198.767C327.867 220.076 381.172 289.295 418.476 331.731C430.674 333.263 467.645 332.786 493.338 309.454C500.482 315.784 512.817 326.835 521.265 336.247C532.428 348.684 539.064 359.266 540.256 362.254C552.206 387.901 532.458 426.283 521.09 442.268L483.111 397.989C446.28 397.879 435.248 426.454 434.335 440.755L469.368 486.841C457.406 492.018 439.568 497.015 432.144 498.867C396.633 504.434 377.714 489.46 372.694 481.277L332.172 419.635C329.572 416.5 328.81 412.687 330.711 411.746L257.307 316.021L249.647 306.032L239.435 292.714C237.23 293.526 233.487 290.731 231.892 289.232C228.764 285.97 217.588 273.409 197.907 249.261C178.226 225.112 180.456 196.216 184.031 184.787C186.127 177.613 191.118 166.449 199.485 154.982C208.92 142.053 222.646 128.74 241.354 120.327C242.873 119.644 244.425 118.994 246.009 118.378C284.431 103.456 308.385 112.097 315.559 118.283Z" fill="black" fill-opacity="0.05"/>
+      <g filter="url(#filter2_f)">
+      <path d="M432.207 499.204C396.696 504.771 377.778 489.797 372.757 481.614C374.473 468.566 373.104 459.837 381.309 440.103C394.469 408.452 416.5 379.073 445.579 364.191C461.5 356.043 503.319 340.363 527.859 343.691C533.851 350.152 536.804 355.047 540.319 362.591C552.27 388.238 532.522 426.62 521.154 442.605L483.175 398.326C446.343 398.216 435.311 426.791 434.399 441.092L469.432 487.178C457.47 492.354 439.631 497.352 432.207 499.204Z" fill="black" fill-opacity="0.05"/>
+      </g>
+      <defs>
+      <filter id="filter0_i" x="0" y="0" width="733.782" height="634.4" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset dy="10"/>
+      <feGaussianBlur stdDeviation="10"/>
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"/>
+      <feBlend mode="normal" in2="shape" result="effect1_innerShadow"/>
+      </filter>
+      <filter id="filter1_i" x="124.682" y="79.2122" width="468.782" height="445.59" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+      <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+      <feOffset dy="-10"/>
+      <feGaussianBlur stdDeviation="10"/>
+      <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1"/>
+      <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+      <feBlend mode="normal" in2="shape" result="effect1_innerShadow"/>
+      </filter>
+      <filter id="filter2_f" x="362.682" y="310.384" width="195.69" height="219.755" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+      <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+      <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+      <feGaussianBlur stdDeviation="2.5" result="effect1_foregroundBlur"/>
+      </filter>
+      <linearGradient id="paint0_linear" x1="573.148" y1="507.832" x2="530.264" y2="546.983" gradientUnits="userSpaceOnUse">
+      <stop stop-color="white"/>
+      <stop offset="1" stop-color="#C4C4C4"/>
+      </linearGradient>
+      </defs>
+    </svg>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Resistor',
+  props: {
+    color1: Number,
+    color2: Number,
+    color3: Number,
+    color4: Number,
+    color5: Number
+  },
+  filters: {
+    convertColor: (color) => {
+      if (color === 0) {
+        return 'black';
+      } else if (color === 1) {
+        return 'brown';  
+      } else if (color === 2) {
+        return 'red';
+      } else if (color === 3) {
+        return 'orange';
+      } else if (color === 4) {
+        return 'yellow';
+      } else if (color === 5) {
+        return 'green';
+      } else if (color === 6) {
+        return 'blue';
+      } else if (color === 7) {
+        return 'violet';
+      } else if (color === 8) {
+        return 'gray';
+      } else if (color === 9) {
+        return 'white';
+      } else if (color === 10) {
+        return 'gold'; 
+      } else if (color === 11) {
+        return 'silver';  
+      } else {
+        return 'red';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.resistor {
+  /* border: green 1px solid; */
+  padding: 20px;
+  border-radius: 16px;
+  -webkit-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
+  -moz-box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
+  box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.5);
+}
+</style>
